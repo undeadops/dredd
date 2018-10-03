3ee9ca2be267ab49fe5ae651f5193db4f996a118
Mitch Anderson <mitch@undeadops.xyz>
2018-10-03 10:45:59 -0600

    changes

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ADD . /code
 WORKDIR /code
 
 RUN pip install -r requirements.txt
+RUN python -m textblob.download_corpora
 
 EXPOSE "5000"
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 ## Dredd - API for determinting Sentiment
 from datetime import datetime
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 
 from textblob import TextBlob
+import unicodedata
 
 app = Flask(__name__)
 api = Api(app)
 
 # Need to Fetch Mongo URL from etcd
-
-
 class JudgeDredd(Resource):
-    def get(self):
+    def post(self):
         queryText = request.form['text']
-        result = TextBlob(queryText)
-        return '{ "sentinment": { "polarity": %s, "subjectivity": %s } }' % (result.sentiment.polarity, result.sentiment.subjectivity)
-
-api.add_resource(JudgeDredd, '/api/v1/')
+        blob = TextBlob(unicodedata.normalize('NFKD', queryText).encode('ascii','ignore').lower())
+        noun_phrases = blob.noun_phrases
+        status = '{ "sentinment": { "polarity": %s, "subjectivity": %s , "noun_phrases": %s } }' % (blob.sentiment.polarity, blob.sentiment.subjectivity, noun_phrases)
+        return jsonify(status), 200
+        
+api.add_resource(JudgeDredd, '/v1/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4500, debug=True)
